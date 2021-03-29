aa94bdba5b3cfadeeb2902a7cf1e8a26545a4fc0
Sahandevs <sahandevs@gmail.com>
2021-03-29 09:46:36 +0430

    add parse_response

--- src/lib.rs
+++ src/lib.rs
@@ -1,61 +1,61 @@
 use std::collections::HashMap;
 
-pub struct Request<'a> {
-    pub method: &'a [u8],
-    pub url: &'a [u8],
+pub struct Response<'a> {
+    pub status: &'a [u8],
+    pub status_code: &'a [u8],
     pub http_version: &'a [u8],
     pub headers: HashMap<&'a [u8], &'a [u8]>,
     pub body: &'a [u8],
 }
 
-enum State {
-    Method,
-    Url,
+enum ResponseParseState {
     HttpVersion,
+    StatusCode,
+    Status,
     Headers { is_end: bool },
     Body,
 }
 
-pub fn parse_request(data: &[u8]) -> Request {
-    let mut state = State::Method;
-    let mut method = 0;
-    let mut url = 0;
+pub fn parse_response(data: &[u8]) -> Response {
+    let mut state = ResponseParseState::HttpVersion;
     let mut http_version = 0;
+    let mut status_code = 0;
+    let mut status = 0;
     let mut header = 0;
     let mut headers_key: Vec<usize> = vec![];
     let mut headers_value: Vec<usize> = vec![];
     for (i, current) in data.iter().enumerate() {
         match state {
-            State::Method => {
+            ResponseParseState::HttpVersion => {
                 if current == &b' ' {
-                    state = State::Url;
-                } else {
-                    method = i;
-                }
-            }
-            State::Url => {
-                if current == &b' ' {
-                    state = State::HttpVersion;
-                } else {
-                    url = i;
-                }
-            }
-            State::HttpVersion => {
-                if current == &b'\n' {
-                    state = State::Headers { is_end: false };
+                    state = ResponseParseState::StatusCode;
                 } else {
                     http_version = i;
                 }
             }
-            State::Headers { is_end } => {
+            ResponseParseState::StatusCode => {
+                if current == &b' ' {
+                    state = ResponseParseState::Status;
+                } else {
+                    status_code = i;
+                }
+            }
+            ResponseParseState::Status => {
+                if current == &b'\n' {
+                    state = ResponseParseState::Headers { is_end: false };
+                } else {
+                    status = i;
+                }
+            }
+            ResponseParseState::Headers { is_end } => {
                 if is_end {
                     if current == &b'\n' {
-                        state = State::Body;
+                        state = ResponseParseState::Body;
                     } else {
                         panic!("invalid state");
                     }
                 } else if current == &b'\r' {
-                    state = State::Headers { is_end: true };
+                    state = ResponseParseState::Headers { is_end: true };
                 } else {
                     if current == &b'\n' {
                         headers_value.push(header);
@@ -68,7 +68,105 @@ pub fn parse_request(data: &[u8]) -> Request {
                     }
                 }
             }
-            State::Body => {
+            ResponseParseState::Body => {
+                break;
+            }
+        }
+    }
+
+    let http_version_slice = &data[..=http_version];
+    let status_code_slice = &data[http_version + 2..=status_code];
+    let status_slice = &data[status_code + 2..=status];
+
+    let mut headers = HashMap::new();
+    let mut last = status + 2;
+    for (key, value) in headers_key.iter().zip(headers_value) {
+        let key_slice = &data[last..=*key];
+        let value_slice = &data[key + 2..=value];
+        last = value + 2;
+        headers.insert(key_slice, value_slice);
+    }
+
+    let body_slice = &data[last + 2..];
+
+    Response {
+        status: status_slice,
+        status_code: status_code_slice,
+        http_version: http_version_slice,
+        headers: headers,
+        body: body_slice,
+    }
+}
+
+pub struct Request<'a> {
+    pub method: &'a [u8],
+    pub url: &'a [u8],
+    pub http_version: &'a [u8],
+    pub headers: HashMap<&'a [u8], &'a [u8]>,
+    pub body: &'a [u8],
+}
+
+enum RequestParseState {
+    Method,
+    Url,
+    HttpVersion,
+    Headers { is_end: bool },
+    Body,
+}
+
+pub fn parse_request(data: &[u8]) -> Request {
+    let mut state = RequestParseState::Method;
+    let mut method = 0;
+    let mut url = 0;
+    let mut http_version = 0;
+    let mut header = 0;
+    let mut headers_key: Vec<usize> = vec![];
+    let mut headers_value: Vec<usize> = vec![];
+    for (i, current) in data.iter().enumerate() {
+        match state {
+            RequestParseState::Method => {
+                if current == &b' ' {
+                    state = RequestParseState::Url;
+                } else {
+                    method = i;
+                }
+            }
+            RequestParseState::Url => {
+                if current == &b' ' {
+                    state = RequestParseState::HttpVersion;
+                } else {
+                    url = i;
+                }
+            }
+            RequestParseState::HttpVersion => {
+                if current == &b'\n' {
+                    state = RequestParseState::Headers { is_end: false };
+                } else {
+                    http_version = i;
+                }
+            }
+            RequestParseState::Headers { is_end } => {
+                if is_end {
+                    if current == &b'\n' {
+                        state = RequestParseState::Body;
+                    } else {
+                        panic!("invalid state");
+                    }
+                } else if current == &b'\r' {
+                    state = RequestParseState::Headers { is_end: true };
+                } else {
+                    if current == &b'\n' {
+                        headers_value.push(header);
+                        header = 0;
+                    } else if current == &b':' {
+                        headers_key.push(header);
+                        header = 0;
+                    } else {
+                        header = i;
+                    }
+                }
+            }
+            RequestParseState::Body => {
                 break;
             }
         }
@@ -103,7 +201,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_parse() {
+    fn test_parse_request() {
         let input = b"GET /index HTTP/1.1\nhost:test.com\nContent-Type:text/html\n\r\nabc";
         let result = parse_request(input);
         assert_eq!(result.method, b"GET");
@@ -116,5 +214,24 @@ mod tests {
             &&b"text/html"[..]
         );
         assert_eq!(result.body, b"abc");
+    }
+
+    #[test]
+    fn test_parse_response() {
+        let input = b"HTTP/1.1 200 OK\nContent-Length:88\nContent-Type:text/html\n\r\nbody123";
+        let result = parse_response(input);
+        assert_eq!(result.status_code, b"200");
+        assert_eq!(result.status, b"OK");
+        assert_eq!(result.http_version, b"HTTP/1.1");
+        assert_eq!(result.headers.len(), 2);
+        assert_eq!(
+            result.headers.get(&b"Content-Length"[..]).unwrap(),
+            &&b"88"[..]
+        );
+        assert_eq!(
+            result.headers.get(&b"Content-Type"[..]).unwrap(),
+            &&b"text/html"[..]
+        );
+        assert_eq!(result.body, b"body123");
     }
 }
